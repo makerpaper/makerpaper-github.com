b573eed50d14b6e6da6cf51a7b43d7cf9d99a7b7
Shemona Singh <singhshemona@gmail.com>
2018-11-11 17:59:23 -0600

    added a few images, sharpened UI, halfway done with categories section

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// // make categories container fixed and scrollable
+
+// window.onscroll = function() {myFunction()};
+
+// var categories = document.querySelector(".categories");
+// var sticky = categories.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     categories.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
