6aac186b06abd3ece13373e8a465524f7f036de1
Shemona Singh <singhshemona@gmail.com>
2018-11-25 18:25:40 -0600

    improved mobile responsiveness for slider feature and blobs

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
 $(".get-started").stick_in_parent();
+
+
+// function checkPosition() {
+//     if (window.matchMedia('(min-width: 865px)').matches) {
+//         $(".get-started").stick_in_parent(); 
+//     }
+//     else {
+//         console.log("hello");
+//     }
+// }
+
+
+// $('.get-started').stick_in_parent({
+//     bottoming: false,
+// });
+
+
+// $('.get-started')
+// .on('sticky_kit:bottom', function(e) {
+//     $('.get-started').css('position', 'absolute');
+//     $('.get-started').css('bottom', '400px');
+// })
+
+// $(".my-sticky-element").trigger("sticky_kit:detach");  
+
+// Media query to turn off sticky-kit
+// if ( $(window).width() > 865) { 
+//     $(".get-started").stick_in_parent();
+// } 
+
