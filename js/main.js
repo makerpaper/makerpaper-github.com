d44082cab5736d18d5ea6123113eb1aebe7194bf
Shemona Singh <singhshemona@gmail.com>
2018-12-20 22:58:22 -0600

    changed favicon, added smooth scrolling, few other refinements

--- js/main.js
+++ js/main.js
@@ -1,34 +1,9 @@
 if($(window).width() >= 820){
     $(".get-started").stick_in_parent();
-    // $('.get-started').css('position', 'absolute');
-    // $('.get-started').css('bottom', '500px');
 }
 
-// function checkPosition() {
-//     if (window.matchMedia('(min-width: 865px)').matches) {
-//         $(".get-started").stick_in_parent(); 
-//     }
-//     else {
-//         console.log("hello");
-//     }
-// }
 
-
-// $('.get-started').stick_in_parent({
-//     bottoming: false,
-// });
-
-
-// $('.get-started')
-// .on('sticky_kit:bottom', function(e) {
-//     $('.get-started').css('position', 'absolute');
-//     $('.get-started').css('bottom', '400px');
-// })
-
-// $(".my-sticky-element").trigger("sticky_kit:detach");  
-
-// Media query to turn off sticky-kit
-// if ( $(window).width() > 865) { 
-//     $(".get-started").stick_in_parent();
-// } 
-
+$( "a.form" ).click(function( event ) {
+    event.preventDefault();
+    $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
+});
