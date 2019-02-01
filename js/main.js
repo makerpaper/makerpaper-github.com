c3cea436a2c334b07a2ccf96bc3c857cf32cccf0
Shemona Singh <singhshemona@gmail.com>
2019-02-01 08:44:35 -0600

    product page complete

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@ if($(window).width() >= 820){
     $(".get-started").stick_in_parent();
 }
 
-
 $( "a.form" ).click(function( event ) {
     event.preventDefault();
     $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
 });
+
+
+
